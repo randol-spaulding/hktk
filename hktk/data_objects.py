5935cf4292b97fd23cd5163f4378738e640e3a7f
Randol Spaulding <rspaulding@somnologymd.com>
2023-02-21 13:30:03 -0800

    Add ability to sort by date

--- hktk/data_objects.py
+++ hktk/data_objects.py
@@ -1,8 +1,12 @@
-from collections import UserList
+from collections import UserList, defaultdict
 from dataclasses import dataclass, field
-from typing import Union, List, Callable
+from typing import Union, List, Callable, Iterable, Optional, TypeVar, Literal
 from datetime import datetime
+from datetime import date as dt_date
 
+
+DType = TypeVar('DType')
+RecordListType = TypeVar('RecordListType')
 
 @dataclass
 class Record:
@@ -35,6 +39,9 @@ class RecordList(UserList, List[Record]):
                 return super().__getitem__(item)
         else:
             raise KeyError(f'Invalid key "{item}" with type "{type(item)}"')
+
+    def sort_by_date(self, date_type: Literal['startDate', 'endDate', 'creationDate'] = 'startDate'):
+        self.sort(key=lambda record: getattr(record, date_type))
 
     def filter(self, func: Callable[[any], bool]) -> 'RecordList':
         filtered_list = RecordList()
