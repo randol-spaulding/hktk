2aa1559a8b434c8e21dd3b872206ec5c2895ddeb
Randol Spaulding <rspaulding@somnologymd.com>
2023-02-22 08:42:09 -0800

    Changing upload handling to cache in temporary cache directory

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -1,16 +1,16 @@
-from hktk.filereader import XMLStringLoader
+from hktk.filereader import XMLStringLoader, XMLLoader
 from dashboard.report_body import get_report_body
 from dash import Dash, html, dcc, Input, Output, State
 from dash.development.base_component import Component
 from dash.exceptions import PreventUpdate
 import uuid
 import base64
-from lxml import etree as ET
+import os
 
 
-def get_layout(app: Dash) -> Component:
+def get_layout(app: Dash, cache_dir: str) -> Component:
     session_id = uuid.uuid4()
-    default_data_store = {'session_id': session_id.hex, 'content': None, 'hk_types': None}
+    default_data_store = {'session_id': session_id.hex, 'cache_dir': cache_dir, 'hk_types': None}
     layout = html.Div(id='content', children=[
         dcc.Store(id='session-store', storage_type='session', data=default_data_store),
         html.H1('HealthKit ToolKit Dashboard'),
@@ -52,19 +52,18 @@ def register_callbacks(app: Dash):
                   Output('selector-div', 'style'),
                   Output('session-store', 'data'),
                   Input('upload-file', 'contents'),
-                  State('upload-file', 'filename'),
-                  State('upload-file', 'last_modified'),
                   State('session-store', 'data'),
                   State('selector-div', 'style'))
-    def update_settings(contents, filename, last_modified, data_store, selector_div_style):
+    def update_settings(contents, data_store, selector_div_style):
         if contents is not None:
             content_type, content_string = contents.split(',')
             contents = base64.b64decode(content_string)
             loader = XMLStringLoader(contents)
-            records = loader.get_all_records()
 
-            data_store['content'] = content_string
-            data_store['hk_types'] = list(records.hk_types)
+            filename = os.path.join(data_store.get('cache_dir'), data_store.get('session_id') + '.xml')
+            loader.save(filename)
+
+            data_store['hk_types'] = list(loader.get_record_type_summary())
             selector_div_style['display'] = 'block'
             return data_store['hk_types'], selector_div_style, data_store
         raise PreventUpdate()
@@ -80,10 +79,8 @@ def register_callbacks(app: Dash):
     def get_report(n_clicks, data_store, hk_types, start_date, end_date) -> Component:
         if n_clicks is None:
             raise PreventUpdate()
-        contents = data_store['content']
-        contents = base64.b64decode(contents)
-
-        loader = XMLStringLoader(contents)
+        filename = os.path.join(data_store.get('cache_dir'), data_store.get('session_id') + '.xml')
+        loader = XMLLoader(filename)
         records = loader.get_all_records_by_type(hk_types)
 
         layout = get_report_body(app, records)
