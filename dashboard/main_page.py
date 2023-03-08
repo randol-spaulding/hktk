c2a41c2bd5838b973d3e003960e2010e9d2b5390
Randol Spaulding <rspaulding@somnologymd.com>
2023-03-07 18:04:00 -0800

    Update dashboard configuration to work specifically for sleep data

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -6,13 +6,23 @@ from dash.exceptions import PreventUpdate
 import uuid
 import base64
 import os
+from datetime import datetime
+import pytz
 
+
+sleep_type_options = [{'label': 'Total duration', 'value': 'duration'},
+                      {'label': 'InBed duration', 'value': 'HKCategoryValueSleepAnalysisInBed'},
+                      {'label': 'REM duration', 'value': 'HKCategoryValueSleepAnalysisAsleepREM'},
+                      {'label': 'Core duration', 'value': 'HKCategoryValueSleepAnalysisAsleepCore'},
+                      {'label': 'Deep duration', 'value': 'HKCategoryValueSleepAnalysisAsleepDeep'},
+                      {'label': 'Wake duration', 'value': 'HKCategoryValueSleepAnalysisAwake'}]
 
 def get_layout(app: Dash, cache_dir: str) -> Component:
     session_id = uuid.uuid4()
     default_data_store = {'session_id': session_id.hex, 'cache_dir': cache_dir, 'hk_types': None}
+
     layout = html.Div(id='content', children=[
-        dcc.Store(id='session-store', storage_type='session', data=default_data_store),
+        dcc.Store(id='session-store', storage_type='local', data=default_data_store),
         html.H1('HealthKit ToolKit Dashboard'),
         dcc.Upload(
             id='upload-file',
@@ -34,7 +44,7 @@ def get_layout(app: Dash, cache_dir: str) -> Component:
         ),
         dcc.Loading([
             html.Div(id='selector-div', style={'display': 'none'}, children=[
-                dcc.Dropdown(id='hk-type-dropdown', multi=True),
+                dcc.Dropdown(id='sleep-type-dropdown', options=sleep_type_options, multi=False),
                 dcc.DatePickerRange(id='date-picker'),
                 html.Button(id='submit-button', children='Submit')
             ])
@@ -48,8 +58,7 @@ def get_layout(app: Dash, cache_dir: str) -> Component:
 
 
 def register_callbacks(app: Dash):
-    @app.callback(Output('hk-type-dropdown', 'options'),
-                  Output('selector-div', 'style'),
+    @app.callback(Output('selector-div', 'style'),
                   Output('session-store', 'data'),
                   Input('upload-file', 'contents'),
                   State('session-store', 'data'),
@@ -65,24 +74,30 @@ def register_callbacks(app: Dash):
 
             data_store['hk_types'] = list(loader.get_record_type_summary())
             selector_div_style['display'] = 'block'
-            return data_store['hk_types'], selector_div_style, data_store
+            return selector_div_style, data_store
         raise PreventUpdate()
 
     @app.callback(
         Output('report-output', 'children'),
         Input('submit-button', 'n_clicks'),
         State('session-store', 'data'),
-        State('hk-type-dropdown', 'value'),
+        State('sleep-type-dropdown', 'value'),
         State('date-picker', 'start_date'),
         State('date-picker', 'end_date')
     )
-    def get_report(n_clicks, data_store, hk_types, start_date, end_date) -> Component:
+    def get_report(n_clicks, data_store, sleep_feature, start_date, end_date) -> Component:
         if n_clicks is None:
             raise PreventUpdate()
         filename = os.path.join(data_store.get('cache_dir'), data_store.get('session_id') + '.xml')
         loader = XMLLoader(filename)
-        records = loader.get_all_records_by_type(hk_types)
+        if start_date and end_date:
+            localizer = pytz.timezone('America/Los_Angeles').localize
+            start_date, end_date = datetime.fromisoformat(start_date), datetime.fromisoformat(end_date)
+            start_date, end_date = localizer(start_date), localizer(end_date)
+            records = loader.get_all_records_in_time_range(start_date, end_date)
+        else:
+            records = loader.get_all_records()
 
-        layout = get_report_body(app, records)
+        layout = get_report_body(app, records, sleep_feature)
 
         return layout
--- dashboard/report_body.py
+++ dashboard/report_body.py
@@ -3,5 +3,5 @@ from dash import Dash, html
 from dash.development.base_component import Component
 
 
-def get_report_body(app: Dash, records: RecordList) -> Component:
-    return html.Div('Report goes here')
+def get_report_body(app: Dash, records: RecordList, sleep_feature: str) -> Component:
+    return html.Div(f'Report for {sleep_feature} goes here')
