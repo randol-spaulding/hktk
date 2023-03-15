b4703142011b53bea69a0092b2f7adc0063e1a60
Randol Spaulding <rspaulding@somnologymd.com>
2023-03-15 16:00:48 -0700

    Changes used to run demo

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -1,29 +1,38 @@
 from hktk.filereader import XMLStringLoader, XMLLoader
 from dashboard.report_body import get_report_body
-from dash import Dash, html, dcc, Input, Output, State
+from dash import Dash, html, dcc, Input, Output, State, dash_table
 from dash.development.base_component import Component
 from dash.exceptions import PreventUpdate
+from plotly import graph_objects as go
 import uuid
 import base64
 import os
-from datetime import datetime
+from datetime import datetime, date as dt_date
 import pytz
+import json
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 
-sleep_type_options = [{'label': 'Total duration', 'value': 'duration'},
-                      {'label': 'InBed duration', 'value': 'HKCategoryValueSleepAnalysisInBed'},
-                      {'label': 'REM duration', 'value': 'HKCategoryValueSleepAnalysisAsleepREM'},
-                      {'label': 'Core duration', 'value': 'HKCategoryValueSleepAnalysisAsleepCore'},
-                      {'label': 'Deep duration', 'value': 'HKCategoryValueSleepAnalysisAsleepDeep'},
-                      {'label': 'Wake duration', 'value': 'HKCategoryValueSleepAnalysisAwake'}]
+sleep_type_options = [{'label': 'Total sleep duration', 'value': 'duration'},
+                      {'label': 'REM duration', 'value': 'REM_duration'},
+                      {'label': 'REM percentage', 'value': 'REM_percent'},
+                      {'label': 'Core duration', 'value': 'Core_duration'},
+                      {'label': 'Core percentage', 'value': 'Core_percent'},
+                      {'label': 'Deep duration', 'value': 'Deep_duration'},
+                      {'label': 'Deep percentage', 'value': 'Deep_percent'},
+                      {'label': 'Wake duration', 'value': 'Awake_duration'},
+                      {'label': 'Wake percentage', 'value': 'Awake_percent'}]
 
 def get_layout(app: Dash, cache_dir: str) -> Component:
-    session_id = uuid.uuid4()
-    default_data_store = {'session_id': session_id.hex, 'cache_dir': cache_dir, 'hk_types': None}
+    # session_id = uuid.uuid4()
+    print(cache_dir)
+    default_data_store = {'session_id': 'export', 'cache_dir': cache_dir, 'hk_types': None}
 
     layout = html.Div(id='content', children=[
-        dcc.Store(id='session-store', storage_type='local', data=default_data_store),
-        html.H1('HealthKit ToolKit Dashboard'),
+        dcc.Store(id='session-store', storage_type='session', data=default_data_store),
+        dcc.Store(id='feature-store', storage_type='session', data=None),
+        html.H1('Sleep Analysis Dashboard'),
         dcc.Upload(
             id='upload-file',
             children=html.Div([
@@ -48,9 +57,18 @@ def get_layout(app: Dash, cache_dir: str) -> Component:
                 dcc.DatePickerRange(id='date-picker'),
                 html.Button(id='submit-button', children='Submit')
             ])
-        ]),
+        ],),
         dcc.Loading([
-            html.Div(id='report-output')
+            html.Div(id='report-output', children=[
+                html.Div(id='table-div', children=[
+                    dash_table.DataTable(id='selector-table', data=None,
+                                         columns=[{'name': 'Field', 'id': 'field'}, {'name': 'Score', 'id': 'score'}],
+                                         style_table={'overflowX': 'auto'})
+                ], style={'display': 'inline-block', 'width': '50%', 'float': 'left'}),
+                html.Div(id='graph-div', children=[
+                    dcc.Graph(id='graph')
+                ], style={'display': 'inline-block', 'width': '50%', 'float': 'right'})
+            ], style={'display': 'none'})
         ])
     ])
     register_callbacks(app)
@@ -65,12 +83,13 @@ def register_callbacks(app: Dash):
                   State('selector-div', 'style'))
     def update_settings(contents, data_store, selector_div_style):
         if contents is not None:
-            content_type, content_string = contents.split(',')
-            contents = base64.b64decode(content_string)
-            loader = XMLStringLoader(contents)
+            # content_type, content_string = contents.split(',')
+            # contents = base64.b64decode(content_string)
 
             filename = os.path.join(data_store.get('cache_dir'), data_store.get('session_id') + '.xml')
-            loader.save(filename)
+            print(filename)
+            loader = XMLLoader(filename)
+            # loader.save(filename)
 
             data_store['hk_types'] = list(loader.get_record_type_summary())
             selector_div_style['display'] = 'block'
@@ -78,26 +97,78 @@ def register_callbacks(app: Dash):
         raise PreventUpdate()
 
     @app.callback(
-        Output('report-output', 'children'),
+        Output('selector-table', 'data'),
+        Output('feature-store', 'data'),
+        Output('report-output', 'style'),
         Input('submit-button', 'n_clicks'),
         State('session-store', 'data'),
         State('sleep-type-dropdown', 'value'),
         State('date-picker', 'start_date'),
         State('date-picker', 'end_date')
     )
-    def get_report(n_clicks, data_store, sleep_feature, start_date, end_date) -> Component:
+    def get_report(n_clicks, data_store, sleep_feature, start_date, end_date) -> tuple[list[dict], dict, dict]:
         if n_clicks is None:
             raise PreventUpdate()
-        filename = os.path.join(data_store.get('cache_dir'), data_store.get('session_id') + '.xml')
+
+        cache_dir = data_store.get('cache_dir')
+        if os.path.exists(os.path.join(cache_dir, sleep_feature + '.json')):
+            with open(os.path.join(cache_dir, sleep_feature + '.json'), 'r') as file:
+                ret = json.load(file)
+            table_data = ret.pop('table_data')
+            return table_data, ret, {'display': 'block'}
+
+        filename = os.path.join(cache_dir, data_store.get('session_id') + '.xml')
         loader = XMLLoader(filename)
-        if start_date and end_date:
-            localizer = pytz.timezone('America/Los_Angeles').localize
-            start_date, end_date = datetime.fromisoformat(start_date), datetime.fromisoformat(end_date)
-            start_date, end_date = localizer(start_date), localizer(end_date)
-            records = loader.get_all_records_in_time_range(start_date, end_date)
-        else:
-            records = loader.get_all_records()
+        start_date = start_date if start_date is not None else datetime(2022, 10, 31, 12, 0, 0).isoformat()
+        end_date = end_date if end_date is not None else datetime.now().isoformat()
 
-        layout = get_report_body(app, records, sleep_feature)
+        localizer = pytz.timezone('America/Los_Angeles').localize
+        start_date, end_date = datetime.fromisoformat(start_date), datetime.fromisoformat(end_date)
+        start_date, end_date = localizer(start_date), localizer(end_date)
+        records = loader.get_all_records_in_time_range(start_date, end_date)
 
-        return layout
+        table_data, x, y, dates = get_report_body(records, sleep_feature)
+
+        with open(os.path.join(cache_dir, sleep_feature + '.json'), 'w') as file:
+            json.dump({'table_data': table_data, 'x': x, 'y': y, 'dates': dates}, file)
+
+        return table_data, {'x': x, 'y': y, 'dates': dates}, {'display': 'block'}
+
+    @app.callback(
+        Output('graph', 'figure'),
+        State('feature-store', 'data'),
+        State('selector-table', 'data'),
+        State('sleep-type-dropdown', 'value'),
+        Input('selector-table', 'active_cell')
+    )
+    def update_graph(feature_data, table_data, dd_state, active_cell):
+        if active_cell is None:
+            raise PreventUpdate()
+        fx, fy, fdates = feature_data['x'], feature_data['y'], feature_data.get('dates', [])
+        feature_name = table_data[active_cell['row']]['field']
+        fy = fy[feature_name]
+        x, y, dates = [], [], []
+        for ind, value in fy:
+            x.append(fx[ind])
+            dates.append(fdates[ind])
+            y.append(value)
+        graph = go.Scatter(x=y, y=x, mode='markers', name='Data',
+                           hovertext=dates)
+
+        m, s = np.mean(y), np.std(y)
+        xn, yn = [], []
+        for xv, yv in zip(x, y):
+            if abs((yv-m)/s) < 3:
+                xn.append(xv)
+                yn.append(yv)
+        lr = LinearRegression()
+        lr.fit(np.expand_dims(yn, -1), np.expand_dims(xn, -1))
+        m0, m1 = min(yn), max(yn)
+        y0, y1 = lr.predict(np.expand_dims([m0], -1)), lr.predict(np.expand_dims([m1], -1))
+        y0, y1 = np.squeeze(y0), np.squeeze(y1)
+
+        fig = go.Figure(data=graph)
+        fig.add_trace(go.Scatter(x=[m0, m1], y=[y0, y1], mode='lines', name='Trendline'))
+
+        fig.update_layout(xaxis_title=' '.join(feature_name.split('_')), yaxis_title=dd_state)
+        return fig
--- dashboard/report_body.py
+++ dashboard/report_body.py
@@ -1,7 +1,94 @@
-from hktk.data_objects import RecordList
-from dash import Dash, html
+from hktk.data_objects import RecordList, SleepStageRecordList
+from dash import Dash, html, dash_table, Input, Output, State, dcc
+from dash.exceptions import PreventUpdate
 from dash.development.base_component import Component
+from datetime import datetime, timedelta, date as dt_date
+from collections import defaultdict
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import tqdm
 
 
-def get_report_body(app: Dash, records: RecordList, sleep_feature: str) -> Component:
-    return html.Div(f'Report for {sleep_feature} goes here')
+def get_report_body(records: RecordList, sleep_feature: str) \
+        -> tuple[list[dict], list[float], dict[str, list[float]], list[str]]:
+
+    sleep_records = records.get_type_subset('HKCategoryTypeIdentifierSleepAnalysis')
+    sleep_blocks: list[SleepStageRecordList] = sleep_records.split_by_sleep_blocks()
+
+    x_features, y_features = [], defaultdict(list)
+
+    all_hk_types = list(records.hk_types)
+    next_records = records.get_type_subset(all_hk_types)
+    next_records.sort_by_date()
+    prev_end = None
+    dates = []
+    print('Collecting data')
+    for i in tqdm.trange(len(sleep_blocks)):
+        sleep_block = sleep_blocks[i]
+        start, end = sleep_block.datetime_range()
+        if prev_end is None:
+            td = timedelta(hours=18)
+        else:
+            td = timedelta(hours=min(24, (start-prev_end).total_seconds()/3600))
+        next_records = next_records[start - td:]
+        before_records = next_records.get_subset_by_date_range(start - td, start, True)
+        # after_records = records[end:end + timedelta(hours=18)]
+
+        feature_summary = sleep_block.get_feature_summary()
+        x_features.append(float(feature_summary[sleep_feature]))
+        dates.append(start.date())
+
+        for hk_type, record_list in before_records.split_by_types().items():
+            name = record_list.hk_simplified_name
+            for feature_name, feature_value in record_list.get_feature_summary().items():
+                if feature_value is None:
+                    continue
+                try:
+                    value = float(feature_value)
+                except:
+                    continue
+                y_features[f'{name}_{feature_name}'].append([i, value])
+        prev_end = end
+
+    scores = {}
+    print('Scoring all data')
+    for feature_id, feature_list in y_features.items():
+        x, y = [], []
+        for index, value in feature_list:
+            x.append(x_features[index])
+            y.append(value)
+
+        if len(x) < 7:
+            print(f'Skipping {feature_id}')
+            continue
+        x, y = np.array(x), np.array(y)
+        x, y = (x - np.mean(x))/np.std(x), (y - np.mean(y))/np.std(y)
+        xn, yn, unique = [], [], set()
+        for xv, yv in zip(x, y):
+            if abs(yv) < 3:
+                xn.append(xv)
+                yn.append(yv)
+                unique.add(yv)
+        if len(unique) <= 2:
+            scores[feature_id] = 0
+            continue
+        xn, yn = np.expand_dims(xn, -1), np.expand_dims(yn, -1)
+
+        lr = LinearRegression()
+        try:
+            lr.fit(yn, xn)
+        except ValueError:
+            print(f'Error with {feature_id}')
+            continue
+        r_squared = lr.score(yn, xn)
+        scores[feature_id] = float(np.squeeze(
+            1/np.sqrt(2) * np.sqrt(
+                r_squared ** 2 + (2*np.arctan(lr.coef_)/np.pi) ** 2
+            )
+        ))
+
+    table_data = []
+    for field_name in sorted(scores, key=lambda k: scores[k], reverse=True):
+        table_data.append({'field': field_name, 'score': scores[field_name]})
+    dates = [date.isoformat() for date in dates]
+    return table_data, x_features, y_features, dates
